334622df45cf98ce5689e9912cd41c1134f0f126
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-07-18 21:51:39 +0800

    Transferred server

---
+++ extra_backend/server.js
@@ -0,0 +1,62 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+const NODE_ENV = process.env.NODE_ENV;
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+        
+        app.listen(PORT, () => {
+            console.log('Extra backend now working.')
+            console.log(`Access at http://localhost:${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.error('MongoDB connection error: ', err);
+        process.exit(1);        
+    });
+
+app.use(express.json())
+
+if(NODE_ENV !== 'production'){
+    app.use(cors());
+    console.log('CORS enebaled for development environment');
+} else {
+    console.log('CORS is NOT enabled for production environment');
+}
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ExTra from the backend');
+});
+
+app.get('/users', (req, res) => {
+    res.send('This is the USERS PAGE');
+})
+
+app.get('/register', (req, res) => {
+    res.send('This is the REGISTRATION PAGE');
+})
+
+app.use((req, res, next) => {
+    res.status(404).send('Sorry the requested resource was not found')
+})
+
+app.use((err, req, res, next) => {
+    console.error('An error has occured: ', err.stack);
+    res.status(500).json({
+        message: 'Internal server error',
+        error: err.message
+    });
+});
