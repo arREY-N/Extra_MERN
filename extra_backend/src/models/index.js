a51e7b435e1a108cc6a6080919e11e5ebcfe5ef4
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-07-20 00:11:39 +0800

    index for all models

---
+++ extra_backend/src/models/index.js
@@ -0,0 +1,11 @@
+import Transaction from "../models/Transaction.js";
+import User from "../models/User.js";
+import Category from "../models/Category.js";
+import Flow from "../models/Flow.js";
+
+export {
+    Transaction,
+    User,
+    Category,
+    Flow
+};
